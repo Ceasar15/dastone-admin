67177e6c102fe1992d21f96f11238527bc2999ce
Ceasar15 <ceasarkwadwo@gmail.com>
2021-10-15 21:01:09 +0000

    auth

--- routes/user.js
+++ routes/user.js
@@ -15,8 +15,37 @@ router.post("/auth-register", async (req, res) => {
     console.log("username:", req.body.username);
     if (foundUser.rows[0]) {
         res.redirect('/auth/register')
+    } else {
+        let hashedPassword = await bcrypt.hash(req.body.password, 10)
+        await db.query('INSERT INTO users (username, password) VALUES ($1, $2)', [req.body.username, hashedPassword])
+        res.redirect('/auth/login')
     }
 })
+
+
+router.get('/login', alreadyAuthenticated, (req, res, next) => {
+    res.render('auth.ejs', {
+      title: 'Login',
+      route: '/auth/login'
+    })
+  })
+
+router.post('/login', passport.authenticate('local', {
+    failureRedirect: '/auth/login',
+    successRedirect: '/secure'
+}))
+
+
+router.get('/logout', (req, res, next) => {
+    if (req.isAuthenticated()) {
+        req.logout()
+        res.redirect('/')
+    } else {
+        res.redirect('back')
+    }
+})
+
+module.exports = router
 
 
 module.exports = router;

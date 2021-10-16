f8f93cfb2ffba89831117f0289b7da3b5c6583d3
Ceasar15 <ceasarkwadwo@gmail.com>
2021-10-16 14:30:00 +0000

    issues bro issues

--- passport.js
+++ passport.js
@@ -58,30 +58,5 @@ passport.use(new localStrategy( async function(username, password, done) {
 // )
 
 
-// passport.use(new localStrategy(
-//     function (username, password, done) {
-//         User.findOne({
-//             username: username
-//         }, function (err, user) {
-//             if (err) {
-//                 return done(err);
-//             }
-//             if (!user) {
-//                 return done(null, false, {
-//                     message: 'Incorrect username.'
-//                 });
-//             }
-//             if (!user.validPassword(password)) {
-//                 return done(null, false, {
-//                     message: 'Incorrect password.'
-//                 });
-//             }
-//             return done(null, user);
-//         });
-//     }
-// ));
-
-
-
 module.exports = passport
 

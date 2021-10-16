ff4c6d39b2bef1c416d89063606d4fc6c3cd8125
Ceasar15 <ceasarkwadwo@gmail.com>
2021-10-16 09:15:43 +0000

    auth

--- passport.js
+++ passport.js
@@ -19,24 +19,24 @@ passport.deserializeUser(async (userID, done) => {
 })
 
 
-passport.use(new localStrategy( async function(username, password, done) {
-    foundUser = await db.query(`SELECT * FROM users WHERE username = $1`, [username])
-    foundUser.rows[0].username.fetch().then(function(data) 
-    //     let foundUser = await db.query('SELECT * FROM users WHERE username = $1', [username])
-    {
-        var user = data;
-        if (user === null) {
-            return done(null, false, { message: 'Invalid username or password' });
-        } else {
+// passport.use(new localStrategy( async function(username, password, done) {
+//     foundUser = await db.query(`SELECT * FROM users WHERE username = $1`, [username])
+//     foundUser.rows[0].username.fetch().then(function(data) 
+//     //     let foundUser = await db.query('SELECT * FROM users WHERE username = $1', [username])
+//     {
+//         var user = data;
+//         if (user === null) {
+//             return done(null, false, { message: 'Invalid username or password' });
+//         } else {
 
-            if (!bcrypt.compareSync(password, foundUser.rows[0].password)) {
-                return done(null, false, { message: 'Invalid password' });
-            } else {
-                return done(null, foundUser.rows[0]);
-            }
-        }
-    });
-}));
+//             if (!bcrypt.compareSync(password, foundUser.rows[0].password)) {
+//                 return done(null, false, { message: 'Invalid password' });
+//             } else {
+//                 return done(null, foundUser.rows[0]);
+//             }
+//         }
+//     });
+// }));
 
 passport.use(new localStrategy(
     async (username, password, done) => {
